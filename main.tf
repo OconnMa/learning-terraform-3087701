6a05e77264531693f5a39294aabefe4b3e886818
OconnMa <141832882+OconnMa@users.noreply.github.com>
2023-08-25 10:17:01 +0100

    update

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "app_ami" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
+  name = "dev"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
@@ -42,15 +42,12 @@ resource "aws_instance" "blog" {
   }
 }
 
-
-
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
   name = "blog"
 
   vpc_id = module.vpc.public_subnets[0]
-
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
