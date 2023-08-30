7f3779ff67ac4ec6dc9a5cd67cc6b3cddb425efe
OconnMa <141832882+OconnMa@users.noreply.github.com>
2023-08-30 10:42:03 +0100

    Update

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -34,20 +34,26 @@ module "vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.10.0"
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  name     = "blog"
+  min_size = 1
+  max_size = 2
 
-  tags = {
-    Name = "Hello World"
-  }
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+
+  image_id            = data.aws_ami.app_ami.id
+  instance_type       = var.instance_type
+
 }
 
 
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
@@ -55,9 +61,9 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.vpc.vpc_id
-  subnets            = module.vpc.public_subnets
-  security_groups    = module.blog_sg.security_group_id
+  vpc_id             = module.blog_vpc.vpc_id[0]
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
@@ -65,12 +71,6 @@ module "alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
     }
   ]
 
@@ -92,7 +92,7 @@ module "blog_sg" {
   version = "5.1.0"
   name = "blog"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -101,4 +101,3 @@ module "blog_sg" {
   egress_rules       = ["all-all"] 
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
-
